c507a41274e1cfc0e68a3795ea395b5ea1b69bca
HoangAnhPham-swnbrn <104686543@student.swin.edu.au>
2026-06-06 15:35:28 +1000

    Update left_right_nowhere.py

--- left_right_nowhere.py
+++ left_right_nowhere.py
@@ -1,24 +1,19 @@
 # Servo Motor Test - Left, Center, Right
 # SG5010 servo on GPIO 13 (pin 33)
 # Raspberry Pi 5
-import RPi.GPIO as GPIO
+import pigpio
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+pi = pigpio.pi()
 
-SERVO = 33
-
-GPIO.setup(SERVO, GPIO.OUT)
-
-pwm = GPIO.PWM(SERVO, 50)  # 50Hz
-pwm.start(0)
+SERVO = 13  # pigpio uses BCM numbering
 
 def set_angle(angle):
-    duty = 5.0 + (angle / 180.0) * 5.0
-    pwm.ChangeDutyCycle(duty)
+    # SG5010: 1000us (0°) to 2000us (180°)
+    pulsewidth = 1000 + (angle / 180.0) * 1000
+    pi.set_servo_pulsewidth(SERVO, pulsewidth)
     sleep(0.5)
-    pwm.ChangeDutyCycle(0)
+    pi.set_servo_pulsewidth(SERVO, 0)  # cut signal
 
 try:
     print("Center")
@@ -26,7 +21,7 @@ try:
     sleep(2)
 
     print("Left")
-    set_angle(60)   # 90 - 30 degrees
+    set_angle(60)
     sleep(2)
 
     print("Center")
@@ -34,7 +29,7 @@ try:
     sleep(2)
 
     print("Right")
-    set_angle(120)  # 90 + 30 degrees
+    set_angle(120)
     sleep(2)
 
     print("Center")
@@ -44,6 +39,5 @@ try:
 except KeyboardInterrupt:
     print("Stopped by user")
 finally:
-    pwm.ChangeDutyCycle(0)
-    pwm.stop()
-    GPIO.cleanup()
+    pi.set_servo_pulsewidth(SERVO, 0)
+    pi.stop()
