199c672973d7d9be40576ea5aa275fa3d6a92e35
HoangAnhPham-swnbrn <104686543@student.swin.edu.au>
2026-06-06 15:14:52 +1000

    Update left_right_nowhere.py#

--- left_right_nowhere.py
+++ left_right_nowhere.py
@@ -1,20 +1,26 @@
 # Servo Motor Test - Left, Center, Right
-# SG5010 servo on GPIO 13 (pin 33, PWM channel 1)
+# SG5010 servo on GPIO 13 (pin 33)
 # Raspberry Pi 5
-from hardware_pwm import HardwarePWM
+import RPi.GPIO as GPIO
 from time import sleep
 
-# GPIO 13 = PWM channel 1, chip=2 for Pi 5
-pwm = HardwarePWM(pwm_channel=1, hz=50, chip=2)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+SERVO = 33
+
+GPIO.setup(SERVO, GPIO.OUT)
+
+pwm = GPIO.PWM(SERVO, 50)  # 50Hz
 pwm.start(0)
 
 def set_angle(angle):
     """0° = left, 90° = center, 180° = right"""
     # SG5010: 1ms (0°) to 2ms (180°) @ 50Hz
     duty = 5.0 + (angle / 180.0) * 5.0
-    pwm.change_duty_cycle(duty)
+    pwm.ChangeDutyCycle(duty)
     sleep(0.5)
-    pwm.change_duty_cycle(0)  # cut signal to reduce jitter
+    pwm.ChangeDutyCycle(0)  # cut signal to reduce jitter
 
 try:
     print("Center")
@@ -40,5 +46,6 @@ try:
 except KeyboardInterrupt:
     print("Stopped by user")
 finally:
-    pwm.change_duty_cycle(0)
+    pwm.ChangeDutyCycle(0)
     pwm.stop()
+    GPIO.cleanup()
